5a596fa7df1e77102a28c203d5eb3a9ea5cf489b
alberteromosele0 <alberteromosele0@gmail.com>
2021-06-17 17:02:43 +0100

    Completed the navigation of root directory (C:\)

--- src/Start-Explorer.ps1
+++ src/Start-Explorer.ps1
@@ -128,20 +128,127 @@ Function ChildFolder {
                }
     }
 } 
-Function Root-Folder{
-    Set-Location C:\
-    Write-Warning "Welcome to The Root Directory."
-    ChildFolder #function 1
-    ChildFolder #function 2
-    ChildFolder #function 3
-    ChildFolder #function 4
-    ChildFolder #function 5
-    ChildFolder #function 6
-    ChildFolder #function 7
-    ChildFolder #function 8
-    ChildFolder #function 9
-    ChildFolder #function 10
+Function RChildDir {
+#listing All Items of Root Folder
+    $RContents = [System.Collections.ArrayList]@()
+    Write-Host "SELECT FROM THE NUMBER BELOW TO OPEN A FOLDER\fILES" -ForegroundColor Green 
+    $RFile_folders = Get-ChildItem .\
+    ForEach($Rfile_folder in $RFile_folders) {
+          $ArrayIndex = $RContents.Add($Rfile_folder)
+          if(($Rfile_folder) -is [System.IO.DirectoryInfo]){
+                Write-Host "$arrayIndex) $Rfile_folder Directory"
+            }
+            Else{Write-Host "$arrayIndex) $Rfile_folder File"}
+        }
+      #Requesting User input
+      $Userinput = Read-Host "ENTER (B) TO GO BACK"
+      if($userInput -eq "B"){
+               #Checking the current location path.
+               $RLocation = Get-Location
+               if($Userinput -eq $RLocation){
+                       Pop-Location
+               } Else {
+                        Clear-Host
+                        Pop-Location
+                        }
+               }
+       Elseif(($RContents[$userInput]) -is [System.IO.DirectoryInfo]) {
+            #Checking if a Directory is empty or not
+            if((Get-ChildItem) -eq $Null){
+                    Write-Host "Sorry, This Directory is empty"
+                    Pop-Location
+                    Write-Host "Redirecting..."
+                    Start-Sleep -Seconds 5
+                    #What should be done if a folder is empty
+         }  Else{
+                Clear-Host
+                $userPath = $RContents[$userInput]
+                Push-Location
+                $Message = Get-Location
+                Clear-Host
+                Write-Host "You Are Currently Working $Message Directory"                
+                }
+        }
+
+       Elseif(($RContents[$userInput]) -is [System.IO.FileInfo]){
+                    $RfilePath = [System.Collections.ArrayList] @()
+                    $GetDirContent = Get-ChildItem .\
+                    ForEach($File in $GetDirContent){
+                        $Null = $RfilePath.Add($file)
+                    }
+
+                    $RfilePath2 = $RfilePath[$userInput]
+                    Write-Host "Processing $filePath2, Please Wait..." -ForegroundColor Green
+                    Invoke-Item -Path .\$RfilePath2
+            }
+       Else {
+                Write-Warning "You Enter The Wrong Key: Enter (B) for Back, Or Enter The Number That Correspond to the Folder/File"
+        }
 }
+Function Root-Folder{
+    Process {
+        Push-Location C:\
+        FolderAction #function
+        $Folders = [System.Collections.ArrayList]@()
+        "`n"
+        Get-ChildItem .\ | ForEach-Object {
+            $null = $folders.Add("$_") #adding folders name to the empty array bucket
+        }
+        $userInput = Read-Host -Prompt "Enter A Number to Open Folder or File"
+        #Checking if value seleted is a file or folder
+        $UserOption = $Folders[$userInput]                 
+        if ((Get-Item -path $userOption) -is [System.IO.DirectoryInfo]) {
+            Try{
+                $UserOption = $folders[$userInput]
+                Push-Location .\$userOption
+                $Message = Get-Location
+                Clear-Host
+                Write-Host "You Are Currently Working $Message Directory"
+
+                #checking if the directory is empty or not, then send a message to user
+                #Error Message Gotten from here.
+                Write-Host "im here"
+                if((Get-ChildItem) -eq $Null){
+                    Write-Host "Sorry, This Directory is empty"
+                    Pop-Location
+                    Write-Host "Redirecting..."
+                    Start-Sleep -Seconds 5
+                    Root-Folder #Function
+                } Else {
+                        #Getting the content of the childItem
+                        RchildDir #function 1
+                        RChildDir #function 2
+                        RChildDir #Function 3
+                        RChildDir #Function 4
+                        RChildDir #Function 5
+                        RChildDir #Function 6
+                        RChildDir #Function 7
+                        RChildDir #Function 8
+                        RChildDir #Function 9
+                        RChildDir #Function 10
+                    }
+                 
+                }
+                Catch [System.ComponentModel.Win32Exception] {
+                    $Error[0].exception
+                }
+                Catch [System.Management.Automation.RuntimeException] {
+                    $Error[0].exception
+                }
+            
+        }
+        Else {
+            Clear-Host
+            Write-Host "Processing $UserOption, Please Wait..." -ForegroundColor Green
+            Invoke-Item -Path .\$UserOption
+            Root-folder #function
+            
+        } #>
+    }
+    End {}
+}
+
+
 Function User-WorkSpace {
     [cmdletbinding()]
     param()
@@ -221,9 +328,8 @@ Function User-WorkSpace {
 }
 
 Function start-UCExplorer{
-    "`n"
+Clear-Host
     Write-Host "Welcome To PowerShell File-Explorer..." -ForegroundColor Green
-    "`n"
     $UserRequest = Read-Host "SELECT: (W) For User-Workspace or (R) For Root Directory"
     "`n"
     switch($UserRequest){
