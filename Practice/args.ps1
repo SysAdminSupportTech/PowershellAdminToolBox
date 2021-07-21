b1dadbbf81e1d749b042a62eaf7a0501c3392324
alberteromosele0 <alberteromosele0@gmail.com>
2021-07-21 22:57:16 +0100

    Update practice folder

---
+++ Practice/args.ps1
@@ -0,0 +1,14 @@
+﻿<#
+Write-Host "Numbers of argument : " ($args.length)
+Write-Output "And they are:"
+foreach($arg in $args){
+    Write-Output $arg
+} #>
+
+param(
+[Parameter(Mandatory = $true, Position = 1, valueFromPipeline=$true)]$Name,
+[Parameter(Mandatory =$true, Position = 2, ValueFromPipeLine = $true)]$LastName)
+
+Write-Output $Name, $LastName
+
+
