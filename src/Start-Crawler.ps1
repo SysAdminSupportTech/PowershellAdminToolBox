5fb6e7b6ed80166b5afc3ff99c9c096b1a57ec65
Albert <EALBERT2@bethel.jw.org>
2022-01-07 10:22:19 +0100

    Start working on Start-Deletion Function

--- src/Start-Crawler.ps1
+++ src/Start-Crawler.ps1
@@ -1,25 +1,35 @@
 function Get-Localdir {
     #Connect to the user computer name 
-    $connect_remote_computer = Get-ChildItem -Path .\ -Directory |
+    $connect_remote_computer = Get-ChildItem -Path .\  |
     ForEach-Object {
         $_.FullName
     }
-    return $connect_remote_computer
+   $connect_remote_computer
 }
-$dirpath = Get-LocalFile
+
 #This function perform action on the directory pass to him by the Get-localfile funtion
 function Start-Action {
-    $ext = [System.Collections.ArrayList]@('.mp4', '.mp3', '.jpg', '.jpeg', '.doc', '.pptx', '.wav', '.wmi', '.pdf', '.msg', '.png', '.xlsx', '.xls')
-    ForEach ($dir in $dirpath) {
-        Write-Output "Directory: $dir"
+    $connect_remote_computer = Get-ChildItem -Path .\
+    $ext = [System.Collections.ArrayList]@('.mp4', '.mp3', '.jpg', '.jpeg', '.doc', '.pptx', '.wav', '.wmi', '.pdf', '.msg', '.png', '.xlsx', '.xls','.vlc')
+    ForEach ($dir in $connect_remote_computer) {
         $content_files = (Get-ChildItem -Path $dir -Recurse).FullName
         ForEach ($file in $content_files) {
             if (((Get-Item -Path $file).Extension) -in $ext) {
-                $file >> Allfiles.txt
+                $file
             }
 
         }
-    }
-        
+    }      
 }
-Start-Action
+
+Function Start-Deletion{
+    param(
+        $FilePath
+    )
+    ForEach($file in $FilePath){
+        Remove-Item $file -WhatIf
+    }
+}
+$dirpath = Get-Localdir
+$all_files = Start-Action
+Start-Deletion -FilePath $All_files
