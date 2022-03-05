075f6d7c42b89cc3f922e7bf29de950babe135ab
Albert <EALBERT@bethel.jw.org>
2022-03-05 17:06:44 +0100

    Script Completed

--- src/Uninstall-UCBlacklistedApplication.ps1
+++ src/Uninstall-UCBlacklistedApplication.ps1
@@ -1,7 +1,16 @@
 ﻿Begin {
     $localHost = (Get-Item -Path Env:\COMPUTERNAME).value
+    $Username = (Get-Item -Path Env:\USERNAME).value #Get the local username runing the script
+    $SuccessLogPath = "\\$localHost\C$\users\$Username\Documents\RoutineMaintaintask\data\Successlog.txt"
     [string]$storePath = New-Item -Path "$env:HOMESHARE\documents\RoutineMaintainTask\Data\" -ItemType Directory -Force
     $UNC = $storePath.replace(":","$")
+
+    #Avoid recreating the Success Save Log file
+    if(-not(Test-Path $storePath\successlog.txt)){
+        $SuccesslogFile = (New-Item -Path $SuccessLogPath -ItemType file).FullName
+    }
+    $SuccesslogFile = "$storePath\Successlog.txt"
+
     #Create a master csv file for applications
     if(Test-Path -Path $storePath\Masterfile.csv){
         Write-Output "Processesing Script Started..."
@@ -26,46 +35,57 @@ Process {
                     Remove-PSSession -ComputerName $Comp
                     #*****************Try/Catch error*******************
                     try {
-                        Write-Host $comp "Online"
                         $tempdir = (New-Item -Path \\$Comp\C$\UCTemp\ -ItemType Directory -Force).FullName #Create a temp file in the local computer using UNC
-
+                        
+                        #Checking if new file exist or not
+                        Write-Output " " >> \\$Comp\C$\UCTemp\Successlogfile.txt
+                        Write-Output "Date: $(Get-Date)" >> \\$Comp\C$\UCTemp\Successlogfile.txt
+                        if(Test-Path -Path $SuccesslogFile){
+                            Write-Output "$Comp Status: Online" >> \\$Comp\C$\UCTemp\Successlogfile.txt
+                            Write-Output "$Comp Status: Online"
+                        }
                         if(-not(Test-Path -Path $tempdir\Masterfile.csv)){
                             Copy-Item -Path \\$localHost\$UNC\Masterfile.csv -Destination $tempdir -force #copy item to the remote computer UCTempdir
                         }
                         Invoke-Command -ComputerName $Comp -ScriptBlock {
                             Push-Location 'C:\Program Files\windowsApps' #set remote Computer path to windowsApp
-                            #$AppsName = @("Spotify", "Minecraft", "minecraftuwp") #List of Blacklisted Application on the computer(line Modified)
                             $AppsName = (Import-Csv -Path c:\uctemp\Masterfile.csv).AppName
                             foreach ($App in $AppsName) {
-                                Write-Host "Checking AppxPackage: $App " -NoNewline
+                                Write-Output "Checking AppxPackage: $App " >> C:\uctemp\Successlogfile.txt
                                 #Uninstalled AppXPackaged application
                                 $AppX = (Get-AppxPackage -AllUsers -Name "*$App*" -PackageTypeFilter Bundle).PackagefullName
                                 if($AppX){
-                                    Write-Host "$AppX"
+                                    Write-Output "$AppX" >> C:\uctemp\Successlogfile.txt
                                     Foreach($AppXPackage in $AppX){
-                                    "`n"
-                                       Write-Host "Removing: $AppXPackage"
-                                       $AppX | Remove-AppxPackage -Confirm:$false
+                                    "`n" >> C:\uctemp\Successlogfile.txt
+                                       Write-Output "Removing: $AppXPackage" >> C:\uctemp\Successlogfile.txt
+                                       $AppX | Remove-AppxPackage -Confirm:$false >> C:\uctemp\Successlogfile.txt
                                     }
                                 } Else {
-                                    Write-Host "Not Found AppXPackage"
+                                    Write-Output "Package Not Found" >> C:\uctemp\Successlogfile.txt
                                 }
                         
                                 #Check for Application Path on Windows Folder and Delete
-                                Write-Host "Checking App: $App " -NoNewline
+                                Write-Output "Checking App: $App " >> C:\uctemp\Successlogfile.txt
                                 $AppsPath = Get-ChildItem -Name "*$app*"
                                 if($AppsPath){
-                                    Write-Host "$AppsPath"
-                                    "`n"
+                                    Write-Output "$AppsPath" >> C:\uctemp\Successlogfile.txt
+                                    "`n" >> C:\uctemp\Successlogfile.txt
                                     ForEach($Path in $AppsPath){
-                                        Remove-Item -Path $Path -Force -Confirm:$false -Recurse -verbose
+                                        Remove-Item -Path $Path -Force -Confirm:$false -Recurse -verbose 3>> C:\uctemp\Successlogfile.txt
                                     }
                                     "`n"
                                 } Else{
-                                    write-host "Not Found"
+                                    Write-Output "Application Path cannot be found Found" >> C:\uctemp\Successlogfile.txt
                                 }
                             }#End of First Foreach
                         }-ErrorAction SilentlyContinue
+
+                        #Copy content from remote computer and delete uctemp folder from remote computers
+                        Get-Content -Path "\\$Comp\C$\uctemp\Successlogfile.txt" >> "\\$localHost\C$\users\$Username\Documents\RoutineMaintaintask\data\Successlog.txt"
+                        Remove-Item -Path \\$Comp\C$\uctemp -Confirm:$false -Recurse
+
+                        #
                     }
                     catch [System.Management.Automation.RemoteException] {
                         $RenameFile = Get-Date -Format "MM/dd/yyyy"
